df6a6e60f432d4711abc30151c3fe65f0cdfbd32
davisilva <84607533+Megueutu@users.noreply.github.com>
2026-09-24 10:51:23 -0300

    feat: prepare npm interface library (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: false,
+  build: {
+    lib: {
+      entry: resolve(import.meta.dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+    },
+  },
 })
